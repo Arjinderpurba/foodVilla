c1f3b894692e4c5a0ff327e400b22ef271109585
Arjinderpurba <arjinderpurba02@gmail.com>
2025-03-03 07:57:57 +0530

    changed footer.js naming

---
+++ app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header.js";
+import Body from "./components/Body.js";
+import Footer from "./components/Footer.js";
+
+//Config Driven UI
+
+
+
+ 
+
+//props - properties
+
+
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+
+        </>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
